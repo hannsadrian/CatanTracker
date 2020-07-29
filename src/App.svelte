85a8c866ba3147ba8b2794582b520573e1d314c5
Adrian <hannsadrian@gmail.com>
2020-07-29 23:41:44 +0200

    Add base functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,40 @@
 <script>
+    import Tailwindcss from "./Tailwindcss.svelte";
+    import Player from "./components/Player.svelte";
+
+    let name = "";
+
+    let max = 13;
+    let players = [];
+
+    function addPlayer(event) {
+        event.preventDefault();
+        players.push({name: name})
+        players = players;
+        name = "";
+    }
 </script>
+
 <Tailwindcss/>
 <main class="text-xl font-semibold">
+    {#if players.length === 0}
+        <p class="italic font-thin">Keine Spieler vorhanden</p>
+    {/if}
+    {#each players as player}
+        <Player {...player} {max}/>
+    {/each}
+    <div class="mt-12 flex justify-between text-base font-normal">
+        <form on:submit={addPlayer}>
+            <input bind:value={name} placeholder="Name"/>
+            <button type="submit">Hinzufügen</button>
+        </form>
+        <div>
+            <p>{max} maximale Siegpunkte</p>
+            <div class="flex">
+                <p class="my-auto mr-2">5</p>
+                <input class="w-3/4" min="5" max="20" bind:value={max} type="range"/>
+                <p class="my-auto ml-2">20</p>
+            </div>
+        </div>
+    </div>
 </main>
---
+++ src/components/Player.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let name;
+    export let max;
+
+    let points = 0;
+
+    function increaseScore() {
+        if (points < max)
+            points++;
+    }
+
+    function decreaseScore() {
+        if (points > 0)
+            points--;
+    }
+
+    $: {
+        if (points > max)
+            points = max;
+    }
+</script>
+
+<div class="flex justify-between my-2">
+    <div>
+        <h2 class="-mb-2 leading-tight">{name}</h2>
+        {#if points >= max}
+            <p class="font-normal">Gewonnen!</p>
+            {:else}
+        <p class="font-thin italic">{points} Punkte</p>
+            {/if}
+    </div>
+    <div class="my-auto flex">
+        <button class="w-8 my-auto mx-2" on:click="{decreaseScore}">-</button>
+        <input type="range" min="0" max="{max}" bind:value={points}/>
+        <button class="w-8 my-auto mx-2" on:click="{increaseScore}">+</button>
+    </div>
+</div>
