d047f07094f1181863e3da7daf4f04bb7c6f1be1
Adrian <hannsadrian@gmail.com>
2020-07-31 12:00:59 +0200

    Start base implementation for static cards

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,55 @@
 <script>
     import Tailwindcss from "./Tailwindcss.svelte";
     import Player from "./components/Player.svelte";
+    import {writable} from "svelte/store";
 
     let name = "";
 
     let max = 13;
     let players = [];
 
+    let onlyOnce = [
+        {id: 0, name: "Längste Handelsstraße", points: 2},
+        {id: 1, name: "Größte Rittermacht", points: 2},
+        {id: 2, name: "Hafenmeister", points: 2},
+        {id: 3, name: "Größter Räuberschreck", points: 2}
+    ]
+
+    function activateCard(id, player, cards) {
+        console.log(id)
+        console.log(player)
+        players.forEach(p => {
+            p.subscribe(s => {
+                console.log(s)
+            })
+        })
+    }
+
     function addPlayer(event) {
         event.preventDefault();
-        players.push({name: name})
+        let cards = [];
+        onlyOnce.forEach(card => {
+            cards.push({...card, active: false})
+            cards = cards;
+        })
+        players.push(writable({name: name, cards: cards}))
         players = players;
         name = "";
     }
 </script>
 
 <Tailwindcss/>
-<main class="text-xl font-semibold">
+<main class="text-xl font-semibold m-2">
     {#if players.length === 0}
         <p class="italic font-thin">Keine Spieler vorhanden</p>
     {/if}
     {#each players as player}
-        <Player {...player} {max}/>
+        <Player {activateCard} {player} {max}/>
     {/each}
     <div class="mt-12 flex justify-between text-base font-normal">
         <form on:submit={addPlayer}>
-            <input bind:value={name} placeholder="Name"/>
-            <button type="submit">Hinzufügen</button>
+            <input bind:value={name} class="rounded px-1 border" placeholder="Name"/>
+            <button class="text-sm text-white bg-blue-600 p-1 rounded" type="submit">Hinzufügen</button>
         </form>
         <div>
             <p>{max} maximale Siegpunkte</p>
--- src/components/Player.svelte
+++ src/components/Player.svelte
@@ -1,8 +1,21 @@
 <script>
-    export let name;
+    import InformationModal from "./InformationModal.svelte";
+
+    export let activateCard;
+    export let player;
     export let max;
 
+    let name = "";
     let points = 0;
+    let modal = false;
+
+    let cards = [];
+
+    function openModal(event) {
+        event.preventDefault();
+        modal = false;
+        modal = true;
+    }
 
     function increaseScore() {
         if (points < max)
@@ -14,24 +27,36 @@
             points--;
     }
 
-    $: {
-        if (points > max)
-            points = max;
-    }
+    player.subscribe(p => {
+        name = p.name;
+        cards = p.cards;
+    })
 </script>
 
-<div class="flex justify-between my-2">
-    <div>
-        <h2 class="-mb-2 leading-tight">{name}</h2>
-        {#if points >= max}
-            <p class="font-normal">Gewonnen!</p>
+<InformationModal title="{name}" shown="{modal}">
+    <div class="text-base font-normal">
+        {#each cards as card}
+            <label on:click={activateCard.bind({}, card.id, name, cards)}>
+                <input type="checkbox" class="my-1" checked={card.active}/>
+                {card.name}
+            </label><br/>
+        {/each}
+    </div>
+</InformationModal>
+<a href on:click={openModal}>
+    <div class="flex justify-between my-2">
+        <div>
+            <h2 class="-mb-2 leading-tight">{name}</h2>
+            {#if points >= max}
+                <p class="font-normal">Gewonnen!</p>
             {:else}
-        <p class="font-thin italic">{points} Punkte</p>
+                <p class="font-thin italic">{points} Punkte</p>
             {/if}
+        </div>
+        <div class="my-auto flex">
+            <button class="w-8 my-auto mx-2" on:click="{decreaseScore}">-</button>
+            <input type="range" min="0" max="{max}" bind:value={points}/>
+            <button class="w-8 my-auto mx-2" on:click="{increaseScore}">+</button>
+        </div>
     </div>
-    <div class="my-auto flex">
-        <button class="w-8 my-auto mx-2" on:click="{decreaseScore}">-</button>
-        <input type="range" min="0" max="{max}" bind:value={points}/>
-        <button class="w-8 my-auto mx-2" on:click="{increaseScore}">+</button>
-    </div>
-</div>
+</a>
