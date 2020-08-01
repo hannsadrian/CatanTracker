c14c0d1cde421e89f3d21d5b82a2d24abf49f5d7
Adrian <hannsadrian@gmail.com>
2020-08-01 22:48:44 +0200

    Fix typos

--- src/App.svelte
+++ src/App.svelte
@@ -12,7 +12,7 @@
         {id: 0, name: "Längste Handelsstraße", points: 2},
         {id: 1, name: "Größte Rittermacht", points: 2},
         {id: 2, name: "Hafenmeister", points: 2},
-        {id: 3, name: "Größter Seeäuberschreck", points: 2},
+        {id: 3, name: "Größter Seeräuberschreck", points: 2},
         {id: -1, name: "BREAK", points: 0},
         {id: 4, name: "Handelsmetropole", points: 2},
         {id: 5, name: "Politikmetropole", points: 2},
